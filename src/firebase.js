a836fef478b3d1825072bb3d5a597ea6b723b148
Mahersi youssef <youssefmahersi0@gmail.com>
2025-03-15 15:45:31 +0100

    update the env variable

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,14 @@
-// firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // your firebase config here
-  apiKey: "AIzaSyC0FKwcQBJ8VfxKBtE7-cRve2rCq2e9tIg",
-  authDomain: "everbloo-front.firebaseapp.com",
-  projectId: "everbloo-front",
-  storageBucket: "everbloo-front.firebasestorage.app",
-  messagingSenderId: "1054206589869",
-  appId: "1:1054206589869:web:8eecc4e10740596ef0bf24",
-  measurementId: "G-1RJ6SSGZMR"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
